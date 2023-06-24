fb2733525e45f4234e4314e5cfa9b2a04feedabb
ogglord <oag@proton.me>
2023-06-25 00:19:26 +0200

    work work

--- configuration.nix
+++ configuration.nix
@@ -39,16 +39,16 @@
     kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
   };
   ## TTY
-  services.kmscon =
-    {
-      enable = true;
-      hwRender = true;
-      #extraConfig =
-      #  ''
-      #    font-name=Lat2-Terminus16
-      #    font-size=14
-      #  '';
-    };
+  #services.kmscon =
+  #  {
+  #    enable = true;
+  #    hwRender = true;
+  #extraConfig =
+  #  ''
+  #    font-name=Lat2-Terminus16
+  #    font-size=14
+  #  '';
+  #  };
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
@@ -57,7 +57,7 @@
     font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
     packages = with pkgs; [ terminus_font ];
     keyMap = "sv-latin1";
-    useXkbConfig = false; # use xkbOptions in tty.
+    useXkbConfig = true; # use xkbOptions in tty.
   };
 
   # Configure keymap in X11
