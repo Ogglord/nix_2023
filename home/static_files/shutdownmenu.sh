15e5f248b7c5099dd2d3a44df89c82d5206e584b
ogglord <oag@proton.me>
2023-06-25 12:09:26 +0200

    switched  shutdown script to rofi

--- home/static_files/shutdownmenu.sh
+++ home/static_files/shutdownmenu.sh
@@ -2,7 +2,7 @@
 
 entries="⇠ Logout\n⏾ Suspend\n󰜉 Reboot\n⏻ Shutdown"
 
-selected=$(echo -e $entries|wofi --width 250 --height 240 --dmenu -p "Menu" --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries|rofi --width 250 --height 240 -dmenu -show window -p "Menu" --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
   logout)
