ee24565862f10a450412e0fccc49294b370bd98d
ogglord <oag@proton.me>
2023-06-25 19:21:25 +0200

    removed rofi_op

--- home/static_files/default.nix
+++ home/static_files/default.nix
@@ -3,14 +3,17 @@
   imports = [ ];
 
   # user friendly cli scripts
+  ## shutdown menu
+
   home.file.".local/bin/shutdownmenu.sh".source = ./shutdownmenu.sh;
-  home.file.".local/bin/rofi-op.sh".source = ./rofi-op.sh;
+
+  ## map applications to icons in sway
   home.file.".config/sworkstyle/config.toml".source = ./sworkstyle_config.toml;
+
+  ## set default apps (browser, file explorer, etc.)
   home.file.".config/mimeapps.list".source = ./mimeapps.list;
 
-
-
-  ## my useful commands
+  ## help command
   home.file.".local/bin/help".source = ./rofi-op.sh;
 
 }
